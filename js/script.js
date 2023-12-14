f8888ada98a630f10560b724682635b1ff6f287c
Banzzai69 <150901470+Banzzai69@users.noreply.github.com>
2023-12-14 10:42:42 +0100

    cv final
    J'ai fait de mon mieux

---
+++ js/script.js
@@ -0,0 +1,51 @@
+function togglePhoneNumber() {
+    var phoneElement = document.getElementById('phone');
+    
+    // If currently hidden, show it; otherwise, hide it
+    if (phoneElement.style.display === 'none' || phoneElement.style.display === '') {
+      phoneElement.style.display = 'block';
+    } else {
+      phoneElement.style.display = 'none';
+    }
+  }
+
+  function toggleAdress() {
+    var adressElement = document.getElementById('adress');
+    
+    // If currently hidden, show it; otherwise, hide it
+    if (adressElement.style.display === 'none' || adressElement.style.display === '') {
+      adressElement.style.display = 'block';
+    } else {
+      adressElement.style.display = 'none';
+    }
+  }
+  function toggleMail() {
+    var mailElement = document.getElementById('mail');
+    
+    // If currently hidden, show it; otherwise, hide it
+    if (mailElement.style.display === 'none' || mailElement.style.display === '') {
+      mailElement.style.display = 'block';
+    } else {
+      mailElement.style.display = 'none';
+    }
+  }
+  function toggleFacebook() {
+    var facebookElement = document.getElementById('facebook');
+    
+    // If currently hidden, show it; otherwise, hide it
+    if (facebookElement.style.display === 'none' || facebookElement.style.display === '') {
+      facebookElement.style.display = 'block';
+    } else {
+      facebookElement.style.display = 'none';
+    }
+  }
+  function toggleInstagram() {
+    var instaElement = document.getElementById('insta');
+    
+    // If currently hidden, show it; otherwise, hide it
+    if (instaElement.style.display === 'none' || instaElement.style.display === '') {
+      instaElement.style.display = 'block';
+    } else {
+      instaElement.style.display = 'none';
+    }
+  }
